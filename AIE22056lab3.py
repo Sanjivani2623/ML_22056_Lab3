37ad7a06c8a680431f858f4a4d07982d634d4a46
Sanjivani2623 <158138898+Sanjivani2623@users.noreply.github.com>
2024-02-15 10:03:24 +0530

    Add files via upload

--- AIE22056lab3.py
+++ AIE22056lab3.py
@@ -1,39 +1,65 @@
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
+import statistics
+import matplotlib.pyplot as plt
 
-def classifier(d,features):
-    features = ["Candies (#)", "Mangoes (Kg)", "Milk Packets (#)"]
-    X = d[features]
-    y = d['Category']
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=40)
-    classifier = DecisionTreeClassifier(random_state=40)
-    classifier.fit(X_train, y_train)
-    d['Predicted'] = classifier.predict(X)
-    return d
+def classifier(d):
+    d["Category"]=data1["Payment (Rs)"].apply(lambda x: 'Rich' if x > 200 else 'Poor')
 
+def purchase(d):
+    featurevec=["Candies (#)","Mangoes (Kg)","Milk Packets (#)"]
+    dataA=d[featurevec]
+    A=dataA.values
+    row,col=A.shape
+    dataC=data1[["Payment (Rs)"]]
+    C=dataC.values
+    rank = np.linalg.matrix_rank(A)
+    pinvA= np.linalg.pinv(A)
+    X=np.dot(pinvA,C)
+
+    return A,C,row,col,rank,pinvA,X
+
+def stock(d):
+    mean = statistics.mean(d['Price'])
+    var = statistics.variance(d['Price'])
+
+    wedn = d[d['Day'] == 'Wed']['Price']
+    wd = pd.to_numeric(wedn)
+    if len(wd)>0:
+        wmean = statistics.mean(wd)
+        wprobProfit = len(wedn[wedn > 0]) / len(wedn)
+        wconProb = len(wedn[wedn > 0]) / len(d[d['Day'] == 'Wed'])
+    else:
+        wmean=None
+        wprobProfit = None
+        wconProb = None
+
+    april = d[d['Month'] == 'Apr']['Price']
+    a = pd.to_numeric(april)
+    if len(wd)>0:
+        aprilmean = statistics.mean(a)
+    else:
+        aprilmean=None
+
+    loss = len(d[d['Chg%'] < 0]) / len(d)
+
+    plt.scatter(d['Day'], d['Chg%'])
+    plt.xlabel('Day of the Week')
+    plt.ylabel('Chg%')
+    plt.title('Chg% vs Day of the Week')
+    plt.show()
+
+    return mean, var, wmean, wprobProfit, wconProb, aprilmean, loss
 
 data1=pd.read_excel(r"C:\Users\Administrator\Downloads\LabSession1Data.xlsx",sheet_name='Purchase data')
-print(data1)
-featurevec=["Candies (#)","Mangoes (Kg)","Milk Packets (#)"]
-dataA=data1[featurevec]
-A=dataA.values
-row,col=A.shape
+A,C,row,col,rank,pinvA,X= purchase(data1)
+
 print(A)
 print("Dimentionality of A : ",col)
 print("Number of vectors: ",row)
-dataC=data1[["Payment (Rs)"]]
-C=dataC.values
-print(C.shape)
-
-rank = np.linalg.matrix_rank(A)
+print(C)
 print("Rank of A :", rank)
-
-pinvA= np.linalg.pinv(A)
 print("Pseudo Inverse of A:\n",pinvA)
-
-X=np.dot(pinvA,C)
 print("X=\n ", X)
 print("Cost of 1 candy: Rs.",round(X[0][0]))
 print("Cost of 1 mango: Rs.",round(X[1][0]))
@@ -42,5 +68,16 @@ print("Cost of 1 milk packet: Rs.",round(X[2][0]))
 data1["Category"]=data1["Payment (Rs)"].apply(lambda x: 'Rich' if x > 200 else 'Poor')
 print(data1)
 
-data1=classifier(data1,featurevec)
+data1=classifier(data1)
 print(data1)
+
+data2=pd.read_excel(r"C:\Users\Administrator\Downloads\LabSession1Data.xlsx",sheet_name='IRCTC Stock Price')
+mean, var, wedn, WProfit, WcondProb, april, loss = stock(data2)
+
+print("Mean of Price data:", mean)
+print("Variance of Price data:", var)
+print("Sample mean of Wednesday prices:", wedn)
+print("Sample mean of April prices:", april)
+print("Probability of loss over the stock:", loss)
+print("Probability of profit on Wednesday:", WProfit)
+print("Conditional probability of profit given that today is Wednesday:", WcondProb)
